ae52a2ec6faa9e53a60081f139a905341d0f8917
Osman Turan <turan@anwalt.de>
2024-09-20 14:37:22 +0200

    add layout

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const { data } = await useFetch('/api/todos')
+
+</script>
+<template>
+  <MainNavbar></MainNavbar>
+  <div class="container mt-10 mx-auto">
+    <pre>{{ data }}</pre>
+  </div>
+</template>
