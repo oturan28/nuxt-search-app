7363daf2064e050d302cff616b4928ad532862c3
Osman Turan <turan@anwalt.de>
2024-12-02 19:14:45 +0100

    add layout

--- pages/internal/index.vue
+++ pages/internal/index.vue
@@ -2,6 +2,8 @@
 definePageMeta({
   middleware: "auth"
 })
+const { clear } = useUserSession()
+
 </script>
 <template>
   <div class="container mt-10 mx-auto">
@@ -9,5 +11,6 @@ definePageMeta({
     <h1>secret internal info!</h1>
     <h1>secret internal info!</h1>
     <h1>secret internal info!</h1>
+    <NuxtLink @click="clear" class="block px-4 py-2 text-sm text-gray-700" to="/logout">Logout</NuxtLink>
   </div>
 </template>
