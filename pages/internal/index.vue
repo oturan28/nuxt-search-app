43cf57d42ca4f7b2a79069e046d0f773389f98fe
Osman Turan <turan@anwalt.de>
2024-12-02 16:00:05 +0100

    add layout

---
+++ pages/internal/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: "auth"
+})
+</script>
+<template>
+  <div class="container mt-10 mx-auto">
+    <h1>secret internal info!</h1>
+    <h1>secret internal info!</h1>
+    <h1>secret internal info!</h1>
+    <h1>secret internal info!</h1>
+  </div>
+</template>
